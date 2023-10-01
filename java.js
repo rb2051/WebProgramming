8b76fd5e345e23a287ed3668024c2de8c9e891ad
rb2051 <rb2051@hw.ac.uk>
2023-10-01 21:04:53 +0100

    javascript draggable items
    i added items that can be dragged and need to make a place for them to be dropped

--- java.js
+++ java.js
@@ -1,7 +1,9 @@
+//changing text on a button with a click
 function demo(){
 const button = document.getElementById("btn").innerHTML = "Button Changed";
 }
 
+//switching automatically between photos
 function carousel() {
   var i;
   var x = document.getElementsByClassName("mySlides");
@@ -14,3 +16,14 @@ function carousel() {
   setTimeout(carousel, 5000); // Change image every 5 seconds
 }
 
+// JavaScript code to handle drag-and-drop functionality
+  var dragItems = document.querySelectorAll('.drag-item');
+
+  dragItems.forEach(function(item) {
+    item.addEventListener('dragstart', function(event) {
+      event.dataTransfer.setData('text/plain', event.target.id);
+    });
+  });
+
+
+  

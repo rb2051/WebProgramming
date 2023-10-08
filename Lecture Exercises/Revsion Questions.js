e445cf295ccc786794bdb3b5dd6d72ab18c562b4
rb2051 <rb2051@hw.ac.uk>
2023-10-08 13:37:23 +0100

    added revision questions
    i added a couple of revision question from the slides to look back at

---
+++ Lecture Exercises/Revsion Questions.js
@@ -0,0 +1,14 @@
+function sum(a, b) {
+      return a + b;
+    }
+
+    sum(1, "2");
+
+//result is "12" due to type coersion
+
+var a = [];
+a[0] = 'test';
+a[3] = 9;
+alert( a );
+
+//result is Array: ['test', , , 9] as the rest of the places in the array have been left undefined
